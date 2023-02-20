04855e0d96c84678beba0212c00296bb9ba9e991
LindaBusra <linda.busra.berg@gmail.com>
2023-02-21 00:26:49 +0100

    new version

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -18,7 +18,7 @@ import org.junit.runner.RunWith;
         features = "./src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
         dryRun = false,
-        tags = "@US_004_TC_001_UI_Testing"
+        tags = "@US012_TC08"
 
 )
 public class Runner {
