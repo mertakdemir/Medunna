db3e0f705b307473940ddd7bd4fcf3fa842712f5
Merve Ozgul <merveozgulmerve@gmail.com>
2023-02-22 20:43:35 +0300

    merve

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -18,7 +18,7 @@ import org.junit.runner.RunWith;
         features = "./src/test/resources/features",
         glue = {"stepdefinitions"},
         dryRun = false,
-        tags = "@US002_TC001_API"
+        tags = "@US_002_Test"
 
 )
 public class TestRunner {
