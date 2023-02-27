a473389f2c1c96742dca9ddb21b1441f0975e247
Mert Sefa Akdemir <akdemirmertsafa@gmail.com>
2023-02-27 11:39:35 +0100

    update

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -18,7 +18,7 @@ import org.junit.runner.RunWith;
         features = "./src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
         dryRun = false,
-        tags = ""
+        tags = "@US001_TC001_API"
 
 
 
