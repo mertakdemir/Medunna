9a0f23d8509c9e690696388fce7a78446c231565
zeynep395 <zynpefe@gmail.com>
2023-03-10 18:02:04 +0000

    z

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -18,7 +18,7 @@ import org.junit.runner.RunWith;
         features = "./src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
         dryRun = false,
-        tags = "@US_025_TC_002"
+        tags = ""
 
 
 
